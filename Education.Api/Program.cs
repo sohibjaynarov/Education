723e3e178aaebbc6f670f605be057d7201097884
Sohib <jaynarovsohib@gmail.com>
2022-04-04 01:14:34 +0500

    add logging

--- Education.Api/Program.cs
+++ Education.Api/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +14,27 @@ namespace Education.Api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            string path = AppDomain.CurrentDomain.BaseDirectory + "Logs\\log.txt";
+            Log.Logger = new LoggerConfiguration().WriteTo.File(
+                    path: path,
+                    outputTemplate: "{Timestamp: yyyy-MM-dd HH:mm:ss } " +
+                    "[{Level:u3}] {Message} {NewLine} {Exception}",
+                    rollingInterval: RollingInterval.Day,
+                    restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information
+                ).CreateLogger();
+            try
+            {
+                Log.Information("Dastur yurdi");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception exception)
+            {
+                Log.Fatal(exception, "Dastur yurishda xatolik bor");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
