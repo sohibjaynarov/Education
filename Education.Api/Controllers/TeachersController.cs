4f5176af2a4aa9b6b0694731ba48a4f3a64b1e8d
Sohib <jaynarovsohib@gmail.com>
2022-04-04 06:43:44 +0500

    add teachers controller

--- Education.Api/Controllers/TeachersController.cs
+++ Education.Api/Controllers/TeachersController.cs
@@ -1,12 +1,67 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Education.Domain.Commons;
+using Education.Domain.Configurations;
+using Education.Service.Interfaces;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+using Education.Domain.Enums;
+using Education.Service.DTOs.Teachers;
+using Education.Domain.Entities.Teachers;
 
 namespace Education.Api.Controllers
 {
-    public class TeachersController : Controller
+    [ApiController]
+    [Route("Api/[controller]")]
+    public class TeachersController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly ITeacherService teacherService;
+        private readonly IWebHostEnvironment env;
+        public TeachersController(ITeacherService teacherService, IWebHostEnvironment env)
         {
-            return View();
+            this.teacherService = teacherService;
+            this.env = env;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<BaseResponse<Teacher>>> Create([FromForm] TeacherForCreationDto teacherDto)
+        {
+            var result = await teacherService.CreateAsync(teacherDto);
+
+            return StatusCode(result.Code ?? result.Error.Code.Value, result);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<BaseResponse<IEnumerable<Teacher>>>> GetAll([FromQuery] PaginationParams @params)
+        {
+            var result = await teacherService.GetAllAsync(@params);
+
+            return StatusCode(result.Code ?? result.Error.Code.Value, result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<BaseResponse<Teacher>>> Get([FromRoute] Guid id)
+        {
+            var result = await teacherService.GetAsync(p => p.Id == id);
+
+            return StatusCode(result.Code ?? result.Error.Code.Value, result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<BaseResponse<Teacher>>> Update(Guid id, TeacherForCreationDto teacherDto)
+        {
+            var result = await teacherService.UpdateAsync(id, teacherDto);
+
+            return StatusCode(result.Code ?? result.Error.Code.Value, result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<BaseResponse<bool>>> Delete(Guid id)
+        {
+            var result = await teacherService.DeleteAsync(p => p.Id == id && p.State != ItemState.Deleted);
+
+            return StatusCode(result.Code ?? result.Error.Code.Value, result);
         }
     }
 }
