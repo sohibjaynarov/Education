c37b5bcc24e8abbac2805402fdd4921c59ba02ff
Sohib <jaynarovsohib@gmail.com>
2022-04-04 06:47:38 +0500

    add Group Controller

--- Education.Api/Controllers/GroupsController.cs
+++ Education.Api/Controllers/GroupsController.cs
@@ -1,12 +1,67 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Education.Domain.Commons;
+using Education.Domain.Configurations;
+using Education.Service.Interfaces;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+using Education.Domain.Entities.Groups;
+using Education.Domain.Enums;
+using Education.Service.DTOs.Groups;
 
 namespace Education.Api.Controllers
 {
-    public class GroupsController : Controller
+    [ApiController]
+    [Route("Api/[controller]")]
+    public class GroupsController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly IGroupService groupService;
+        private readonly IWebHostEnvironment env;
+        public GroupsController(IGroupService groupService, IWebHostEnvironment env)
         {
-            return View();
+            this.groupService = groupService;
+            this.env = env;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<BaseResponse<Group>>> Create([FromForm] GroupForCreationDto groupDto)
+        {
+            var result = await groupService.CreateAsync(groupDto);
+
+            return StatusCode(result.Code ?? result.Error.Code.Value, result);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<BaseResponse<IEnumerable<Group>>>> GetAll([FromQuery] PaginationParams @params)
+        {
+            var result = await groupService.GetAllAsync(@params);
+
+            return StatusCode(result.Code ?? result.Error.Code.Value, result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<BaseResponse<Group>>> Get([FromRoute] Guid id)
+        {
+            var result = await groupService.GetAsync(p => p.Id == id);
+
+            return StatusCode(result.Code ?? result.Error.Code.Value, result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<BaseResponse<Group>>> Update(Guid id, GroupForCreationDto groupDto)
+        {
+            var result = await groupService.UpdateAsync(id, groupDto);
+
+            return StatusCode(result.Code ?? result.Error.Code.Value, result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<BaseResponse<bool>>> Delete(Guid id)
+        {
+            var result = await groupService.DeleteAsync(p => p.Id == id && p.State != ItemState.Deleted);
+
+            return StatusCode(result.Code ?? result.Error.Code.Value, result);
         }
     }
 }
